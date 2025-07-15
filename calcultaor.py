2de40d150e69be0e3147b21f75de05ff2c462adf
Hemayat-Khan <hemayaturrehman50@gmail.com>
2025-07-15 11:16:09 +0400

    This is simple calculator in python language

---
+++ calcultaor.py
@@ -0,0 +1,14 @@
+print("----Calculator----")
+for i in range(5):
+    a= int(input("Enter your first value:"))
+    b= int(input("Enter your second value:"))
+    A= a+b
+    B= a-b
+    C= a*b
+    D= a/b
+    print("Addition is:",A)
+    print("Subraction is:",B)
+    print("multiplication is:",C)
+    print("Division",D)
+
+print("Code Ended Thanks")
